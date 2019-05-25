544b2a14b7ecf737c5786832ff88e4126eb5ef63
Arman Murzabulatov <kz.arman@gmail.com>
2019-05-25 10:23:10 +0200

    set endpoint configuration // temporarily

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    giphy: {
+      baseUrl: 'https://api.giphy.com/v1/gifs/search',
+      apiKey: 'CdRKiCMbTnt9CkZTZ0lGukSczk6iT4Z6'
+    }
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    giphy: {
+      baseUrl: 'https://api.giphy.com/v1/gifs/search',
+      apiKey: 'CdRKiCMbTnt9CkZTZ0lGukSczk6iT4Z6'
+    }
+  }
 };
 
 /*
@@ -13,4 +19,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
