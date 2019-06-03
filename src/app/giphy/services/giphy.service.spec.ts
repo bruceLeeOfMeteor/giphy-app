298bf275a490837289e636b87adf19cae552bfb1
Arman Murzabulatov <kz.arman@gmail.com>
2019-06-03 11:21:10 +0200

    minor: rewire environment for service spec file

--- src/app/giphy/services/giphy.service.spec.ts
+++ src/app/giphy/services/giphy.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 import { GiphyService } from './giphy.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
